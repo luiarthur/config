35fe5b89bf6ddd297745f459eb500b02d962416e
Arthur Lui <luiarthur@gmail.com>
2018-05-06 19:38:50 -0700

    Added install instructions for java

--- setup/ubuntu/install.sh
+++ setup/ubuntu/install.sh
@@ -9,3 +9,16 @@ sudo apt install python-pip
 
 # Install Google Calendar Command-line Client
 pip install gcalcli
+
+# Latex
+### Install Basic Latex ###
+sudo apt install texlive-latex-extra
+### Install special fonts (e.g. bbm.sty) ### 
+sudo apt install texlive-fonts-extra
+
+# R 
+sudo apt install r-base-core
+
+# Java
+sudo apt install default-jre
+sudo apt install default-jdk
