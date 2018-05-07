a80852d7a47435cf078d1d0f96b9c989317a0387
Arthur Lui <luiarthur@gmail.com>
2018-05-06 18:40:55 -0700

    minor changes

--- setup/ubuntu/install.sh
+++ setup/ubuntu/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # For recompiling C / C++ (especially Rcpp)
-sudo apt-get install ccache
+sudo apt get install ccache
 
 # Install pip
-sudo apt-get update
-sudo apt-get install python-pip
+sudo apt get update
+sudo apt install python-pip
 
 # Install Google Calendar Command-line Client
 pip install gcalcli
