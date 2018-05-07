66666dae185212f3b9a427c6ada22694df5052b7
Arthur Lui <luiarthur@gmail.com>
2018-05-06 23:10:14 -0700

    yes

--- setup/ubuntu/install.sh
+++ setup/ubuntu/install.sh
@@ -40,6 +40,7 @@ echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.li
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 sudo apt update
 sudo apt install -y sbt
+# See notes in `../osx/install.sh` for what to do if behind firewall.
 
 # For recompiling C / C++ (especially Rcpp)
 sudo apt install -y ccache
