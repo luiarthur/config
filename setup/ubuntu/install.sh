f230d21e27d81cfbb92a449f050bd1741a071412
Arthur Lui <luiarthur@gmail.com>
2018-05-06 22:35:07 -0700

    yes

--- setup/ubuntu/install.sh
+++ setup/ubuntu/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# For recompiling C / C++ (especially Rcpp)
-sudo apt install -y ccache
+sudo apt update
+
+# Gnome-tweaks (for system settings)
+sudo apt install -y gnome-tweaks
 
 # Install pip
-sudo apt update
 sudo apt install -y python-pip
 
 # Install Google Calendar Command-line Client
@@ -26,8 +27,16 @@ sudo apt install -y r-base-core
 sudo apt install default-jre
 sudo apt install default-jdk
 
+# Scala
+sudo apt install scala
+
 # SBT
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 sudo apt update
 sudo apt install sbt
+
+# For recompiling C / C++ (especially Rcpp)
+sudo apt install -y ccache
+
+
