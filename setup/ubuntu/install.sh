ea50204c34607e324dd406ed4e3cd0126bbcd105
Arthur Lui <luiarthur@gmail.com>
2018-05-06 19:41:33 -0700

    yes

--- setup/ubuntu/install.sh
+++ setup/ubuntu/install.sh
@@ -22,3 +22,9 @@ sudo apt install r-base-core
 # Java
 sudo apt install default-jre
 sudo apt install default-jdk
+
+# SBT
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt update
+sudo apt install sbt
