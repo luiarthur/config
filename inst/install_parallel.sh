47587f137d0e646bd524bfebccecc1b489cd44bd
Arthur Lui <luiarthur@gmail.com>
2018-08-06 10:48:44 -0700

    yes

--- inst/install_parallel.sh
+++ inst/install_parallel.sh
@@ -3,4 +3,15 @@
 # See the great excuses to not install GNU parallel
 # http://oletange.blogspot.com/2013/04/why-not-install-gnu-parallel.html
 
+# This is where the package will be downloaded to
+tmp_dir="$HOME/tmp_install_parallel_here_blablablablabla_ihopeineverusethisdir/"
+mkdir -p $tmp_dir
+cd $tmp_dir
+
+# Installs parallel to ~/bin
 (wget pi.dk/3 -qO - ||  curl pi.dk/3/) | bash
+
+# remove the installation files
+cd $HOME
+rm -rf $tmp_dir
+
