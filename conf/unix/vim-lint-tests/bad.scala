30529c5c62732b3ab47f12da6182d9117ef4299a
Arthur Lui <luiarthur@gmail.com>
2018-09-01 13:34:33 -0700

    added some bad example

--- conf/unix/vim-lint-tests/bad.scala
+++ conf/unix/vim-lint-tests/bad.scala
@@ -1,7 +1,7 @@
 import math._
 
 object Bla {
-  val x = 1
+  val x = 1; val x = 1; val x = 1; val x = 1; val x = 1; val x = 1; val x = 1; val x = 1; val x = 1; val x = 1; val x = 1; val x = 1; 
 }
 
 println(Bla.x)
