0c315ba03ca81719925025fb524eb3ce711eafe1
Arthur Lui <arthur.lui@climate.com>
2019-07-01 15:56:28 -0700

    use python3 for vulture

--- bin/unix/vulture.py
+++ bin/unix/vulture.py
@@ -1,28 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys, os, time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 if len(sys.argv) < 3:
-    print "Usage:\n"
-    print "$ vulture   fileToWatch    cmdToExecute"
+    print("Usage:\n")
+    print("$ vulture   fileToWatch    cmdToExecute")
     sys.exit(1)
 
 fullpath = sys.argv[1]
-cmd = ' '.join( str(s) for s in sys.argv[2:] )
+cmd = ' '.join(str(s) for s in sys.argv[2:])
 path = "." if os.path.dirname(fullpath) == "" else os.path.dirname(fullpath) + "/"
 fname = os.path.basename(fullpath)
 ext = os.path.splitext(fname)[1]
 
 os.system (cmd)
-print "######################### Changes made at: "+ path + "/" + fname +" #########################"
+print("######################### Changes made at: "+ path + "/" + fname +" #########################")
 
 class MyHandler(PatternMatchingEventHandler):
-    patterns = ["*"+ext]
+    patterns = ["*" + ext]
     def on_created(self, event):
         os.system (cmd)
-        print "######################### Changes made at: "+event.src_path +" #########################"
+        print("######################### Changes made at: "+event.src_path +" #########################")
 
 observer = Observer()
 observer.schedule(MyHandler(), path, recursive=False)
