0f36ddf67c900affa0b65bcdd42d9d72d7ccc77f
Arthur Lui <luiarthur@gmail.com>
2020-04-27 18:23:35 -0700

    updated install.sh

--- setup/wsl/install.sh
+++ setup/wsl/install.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
 # Update repositories
+echo "Preparing for installs"
 sudo apt update && sudo apt upgrade
 
 # Install tmux related plugins. tmux ships with wsl.
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# test -f ~/.tmux.conf && tmux source ~/.tmux.conf
+echo "tmux installations ..."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+test -f ~/.tmux.conf && tmux source ~/.tmux.conf
 
 # Install better vim
-# sudo apt install -y vim-gnome
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Vim installations ..."
+sudo apt install -y vim-gnome
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install pip
-# sudo apt install -y python3-pip
+echo "pip installations ..."
+sudo apt install -y python3-pip
 
 # Install Latex
+echo "latex installations ..."
 source install-latex.sh
 
 # Install R
+echo "R installations ..."
 sudo apt install -y r-base-core
 
 # For recompiling C / C++ (especially Rcpp)
+echo "ccache installations ..."
 sudo apt install -y ccache
 
 # Install scala
+# echo "scala installations ..."
 # source install-scala.sh
 
 # TODO: Install julia from julia website
