013dd9cbc9c9b4a177e923a54d36bfedffe791c6
Arthur Lui <alui@lanl.gov>
2021-12-06 10:25:09 -0700

    added todo items

--- v1.0/src/os/linux-server/install.sh
+++ v1.0/src/os/linux-server/install.sh
@@ -10,14 +10,19 @@ then
   rm -rf ~/tmp/conda-install
 fi
 
+
 # Install tree if not installed.
+# TODO: Get latest appimage?
 [[ `which tree` ]] || conda install -c conda-forge tree
 
 # Install htop if not installed.
+# TODO: Get latest appimage?
 [[ `which htop` ]] || conda install -c conda-forge htop
 
 # Install neovim if not installed.
-[[ `which neovim` ]] || conda install -c conda-forge neovim
+# [[ `which neovim` ]] || conda install -c conda-forge neovim
+[[ `which neovim` ]] || wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O ~/bin/nvim
+chmod +x ~/bin/nvim
 
 # Download nvim-config and setup.
 git clone https://github.com/luiarthur/nvim-config ~/repo/nvim-config
@@ -25,7 +30,10 @@ cd ~/repo/nvim-config && make install
 cd $current_dir
 
 # Install tmux if not installed.
-[[ `which tmux` ]] || conda install -c conda-forge tmux
+# [[ `which neovim` ]] || conda install -c conda-forge tmux
+# TODO: Get latest appimage?
+[[ `which tmux` ]] || wget https://github.com/tmux/tmux/releases/download/3.1b/tmux-3.1b-x86_64.AppImage -O ~/bin/tmux
+chmod +x ~/bin/tmux
 
 # Install tmux plugin manager.
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
