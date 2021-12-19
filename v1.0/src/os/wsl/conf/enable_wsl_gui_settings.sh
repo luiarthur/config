f2a8dfa9644e727ebef6f4aa27267a5d0fb0e354
Arthur Lui <luiarthur@gmail.com>
2021-12-19 15:35:30 -0700

    updated settings for wsl

---
+++ v1.0/src/os/wsl/conf/enable_wsl_gui_settings.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+### WSL 2 ###
+configure_wsl_specific_settings() {
+  # NOTE: Not sure if any of these are still needed since LINUX GUI apps are
+  # officially supported in WSL2. I think all of these are set by default in
+  # the newer wsl2 versions.
+
+  # For VcXsrv (WSL2)
+  LOCAL_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
+  export DISPLAY=$LOCAL_IP:0
+  export LIBGL_ALWAYS_INDIRECT=1
+
+  # WSL, X11 stuff. For matplotlib, seaborn.
+  export NO_AT_BRIDGE=1
+
+  # NOTE: Fixes default permissions of new directories in WSL.
+  # Source: https://www.turek.dev/post/fix-wsl-file-permissions/
+  [[ "$(umask)" = "0000" ]] && umask 0022
+}
+
+# NOTE: If things break, add the following in ~/.bashrc:
+# 
+#     source configure_wsl_specific_settings.sh
+#     configure_wsl_specific_settings
